6c2e3f0a1a661e53fc718d56cd30c954b9f23f11
ltomada <ltomada91@gmail.com>
2021-01-28 03:29:21 +0100

    1 Iterazione

---
+++ Assets/awdawdawd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class awdawdawd : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Vector3 objectPos;
+    public GameObject yourPrefab;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            mousePos = Input.mousePosition;
+            mousePos.z = 2.0f;
+            objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Instantiate(yourPrefab, objectPos, Quaternion.identity);
+        }
+    }
+}
+
+
+
