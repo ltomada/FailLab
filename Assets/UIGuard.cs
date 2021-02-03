5fc7b5a88b83f7bfa605f2df14649dff96eb8d96
ltomada <ltomada91@gmail.com>
2021-02-03 05:31:11 +0100

    wads

---
+++ Assets/UIGuard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIGuard : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+            {
+                GameObject.CreatePrimitive(PrimitiveType.Cube)
+                    .transform.position = hitInfo.point;
+            }
+        }
+    }
+}
